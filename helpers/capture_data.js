57029659c12b7a37b0c1f62e4a1350c48bbe082a
Christopher Fairfax <cfairfax2010@gmail.com>
2018-10-17 08:01:15 -0400

    Getting Form capture data reconfigured to match To() pattern

--- helpers/capture_data.js
+++ helpers/capture_data.js
@@ -1,25 +1,29 @@
 const parseXML = require('xml2js-es6-promise');
-const parseXMLSync = require('xml2js-parser').parseStringSync;
 const Promise = require('bluebird');
 const To = require('./To.js');
 
 module.exports = async (z, bundle) => {
-    // Refactor to async/await To(promise) pattern - easier to read - bundle errors in single try/catch
-    return z.request({
-        url: 'https://' + bundle.authData.subdomain + '.clickdimensions.com/Service.svc/v1/account/' + bundle.authData.account_key + '/captures/' + bundle.inputData.form_capture_id,
-        method: 'GET'
-    }).then((res) => {
+    // Get the formcapture's Action URL, and Success/Error redirect URLs
+    try {
+        var [getErr, res] = await To(z.request({
+            url: 'https://' + bundle.authData.subdomain + '.clickdimensions.com/Service.svc/v1/account/' + bundle.authData.account_key + '/captures/' + bundle.inputData.form_capture_id,
+            method: 'GET'
+        }));
+        if(getErr) throw new Error(getErr)
+
         res.throwForStatus();
-        return parseXMLSync(res.content);
-    }).then((data) => {
+        var [parseErr, data] = await To(parseXML(res.content));
+        if(parseErr) throw new Error(parseErr);
+
         var location = data.FormCapture.Location[0].split(':');
         if(location[0] !== 'https') {
             location[0] = 'https'
         }
         location = location.join(':');
+        
         var success = data.FormCapture.SuccessLocation[0];
         var error = data.FormCapture.ErrorLocation[0];
-        
+            
         var captureFields = [
             {
                 key: 'post_url',
@@ -43,7 +47,10 @@ module.exports = async (z, bundle) => {
                 default: error
             }
         ];
-        
+            
         return captureFields;
-    })
+    }
+    catch (err) {
+        throw new Error(err);
+    }
 }
